f437a0676e00c092ef48629d14d89580c889e031
Stephen Ancliffe <stephen.ancliffe@gmail.com>
2024-05-06 20:30:18 -0400

    updated connection logic

--- KodiGetMoviesNoSQL.sh
+++ KodiGetMoviesNoSQL.sh
@@ -1,10 +1,43 @@
 #!/bin/bash
 
-# Kodi credentials
-kodi_username="kodi"
-kodi_password="DogRunFast"
-kodi_UserPassword="$kodi_username:$kodi_password"
-kodi_host="http://192.168.2.79:8181/jsonrpc"
+# Function to build the Kodi connection URL
+kodi_connect_url() {
+  local protocol="http"  # You can modify this if needed (e.g., https)
+  local ip="$1"
+  local port="$2"
+  echo "$protocol://${ip}:${port}/jsonrpc"
+}
+
+# Function to build the Kodi authorization header
+kodi_auth_header() {
+  local username="$1"
+  local password="$2"
+  local encoded_credentials=$(echo -n "$username:$password" | base64)
+  echo "Authorization: Basic $encoded_credentials"
+}
+
+# Process command-line arguments
+while getopts ":u:p:i:P:" opt; do
+  case $opt in
+    u) kodi_username="$OPTARG" ;;
+    p) kodi_password="$OPTARG" ;;
+    i) kodi_ip="$OPTARG" ;;
+    P) kodi_port="$OPTARG" ;;
+    \?) echo "Invalid option: -$OPTARG" >&2; exit 1 ;;
+  esac
+done
+
+# Remove processed options from $@
+shift $((OPTIND-1))
+
+# Check if required arguments are provided
+if [[ -z "$kodi_username" || -z "$kodi_password" || -z "$kodi_ip" || -z "$kodi_port" ]]; then
+  echo "Error: Missing required arguments. Please provide -u username, -p password, -i IP address, and -P port."
+  exit 1
+fi
+
+kodi_url=$(kodi_connect_url "$kodi_ip" "$kodi_port")
+auth_header=$(kodi_auth_header "$kodi_username" "$kodi_password")
 
 # Function to build the JSON request
 GetMovies() {
@@ -15,7 +48,7 @@ GetMovies() {
 json_req=$(GetMovies)
 
 # Send request using curl and capture the response
-response=$(curl -X POST -s -H 'Content-Type: application/json' "$kodi_host" -u "$kodi_UserPassword" -d "$json_req")
+response=$(curl -X POST -s -H 'Content-Type: application/json' -H "$auth_header" "$kodi_url" -d "$json_req")
 
 # Check if curl was successful (exit code 0 indicates success)
 if [ $? -ne 0 ]; then
@@ -43,3 +76,6 @@ done
 # Print the lower and upper IDs
 echo "The lower ID is: ${movie_id_array[0]}"
 echo "The upper ID is: ${movie_id_array[${#movie_id_array[@]} - 1]}"
+
+
+
